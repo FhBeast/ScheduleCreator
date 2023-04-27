ac0e145242c3e08db98f1d81a1200b934c790f7a
FhBeast <fhdavidqqq@gmail.com>
2023-04-27 09:07:32 +0300

    add Timetable.cs

---
+++ Schedule/Entities/Timetable.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Schedule.Entities;
+
+[Index(nameof(TimetableName), IsUnique = true)]
+public class Timetable : BaseEntity
+{
+    public string TimetableName { get; set; }
+    public string? Weekend { get; set; }
+    public List<Shift> Shifts { get; set; }
+    public List<Employee> Employees { get; set; }
+}
