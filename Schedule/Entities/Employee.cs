55885538bfe02709c9f50e2bd77f6fdd4faafa7f
FhBeast <fhdavidqqq@gmail.com>
2023-04-27 09:07:22 +0300

    add Employee.cs

---
+++ Schedule/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schedule.Entities;
+public class Employee : BaseEntity
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Surname { get; set; }
+    public string? Email { get; set; }
+    public int ExceedingWorkingDays { get; set; }
+    public string? WorkDays { get; set; }
+}
