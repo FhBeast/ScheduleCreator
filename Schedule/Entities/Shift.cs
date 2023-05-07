a012866eafc19b35ab4497fbe562758376eaa450
FhBeast <fhdavidqqq@gmail.com>
2023-05-07 20:52:08 +0300

    add property EmployeesNumber

--- Schedule/Entities/Shift.cs
+++ Schedule/Entities/Shift.cs
@@ -1,7 +1,8 @@
 ﻿namespace Schedule.Entities;
 public class Shift : BaseEntity
 {
-    public string Start { get; set; }
-    public string End { get; set; }
-    public bool IsQuaranteeWeekend;
+    public string? Start { get; set; }
+    public string? End { get; set; }
+    public int EmployeesNumber { get; set; }
+    public bool IsQuaranteeWeekend { get; set; }
 }
