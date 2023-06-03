2303acaa943a0590f1507824b8651f6b94adf41f
FhBeast <fhdavidqqq@gmail.com>
2023-06-03 20:03:15 +0300

    add TimetableSearchViewModel

---
+++ Schedule/ViewModels/Timetable/TimetableSearchingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Schedule.ViewModels.Employee;
+using Schedule.ViewModels.Shift;
+using System.ComponentModel.DataAnnotations;
+using Schedule.Entities;
+
+namespace Schedule.ViewModels.Timetable;
+public class TimetableSearchingViewModel
+{
+    [Display(Name = "Название расписания")]
+    public string? TimetableName { get; set; }
+
+    [Display(Name = "Владелец")]
+    public string? Owner { get; set; }
+
+    [Display(Name = "Количество смен")]
+    public int ShiftsNumber { get; set; }
+
+    [Display(Name = "Количество работников")]
+    public int EmployeesNumber { get; set; }
+}
