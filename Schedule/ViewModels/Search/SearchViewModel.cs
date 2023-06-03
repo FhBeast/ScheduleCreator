95d510c77c01926f82710059481d23dc09dea302
FhBeast <fhdavidqqq@gmail.com>
2023-06-03 20:02:41 +0300

    add SearchViewModel

---
+++ Schedule/ViewModels/Search/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Schedule.ViewModels.Employee;
+using Schedule.ViewModels.Shift;
+using System.ComponentModel.DataAnnotations;
+using Schedule.Entities;
+using Schedule.ViewModels.Timetable;
+
+namespace Schedule.ViewModels.Search;
+public class SearchViewModel
+{
+    [Required(ErrorMessage = "Поле {0} обязательно для ввода.")]
+    [StringLength(50)]
+    [Display(Name = "Поиск")]
+    public string? SearchText { get; set; }
+    public List<TimetableSearchingViewModel>? TimetableSearchingViewModels { get; set; }
+}
