3a2d617acb87d230899552459e195546b2bf9483
FhBeast <fhdavidqqq@gmail.com>
2023-06-03 20:03:01 +0300

    add TimetableEmployeeViewModel

---
+++ Schedule/ViewModels/Timetable/TimetableEmployeeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Schedule.ViewModels.Employee;
+using Schedule.ViewModels.Shift;
+using System.ComponentModel.DataAnnotations;
+using Schedule.Entities;
+
+
+namespace Schedule.ViewModels.Timetable;
+public class TimetableEmployeeViewModel
+{
+    [Required(ErrorMessage = "Поле {0} обязательно для ввода.")]
+    [StringLength(50)]
+    [Display(Name = "Название расписания")]
+    public string? TimetableName { get; set; }
+
+    [Display(Name = "Владелец")]
+    public string? Owner { get; set; }
+
+    public EmployeeViewModel? Employee { get; set; }
+
+    public List<ShiftViewModel>? ShiftViewModels { get; set; }
+}
